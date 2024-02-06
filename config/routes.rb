1c236f8fa7d6a179cc273a2e7db932a7ba7ef021
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-06 22:18:48 +0000

    set up square and results route and template files for others

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  
+  get("/", { :controller => "omni", :action => "sq" })
+  get("/square/results", { :controller => "omni", :action => "sq_result" })
+  get("/square/new", { :controller => "omni", :action => "sq" })
+
+  get("/squarert", { :controller => "omni", :action => "sqrt" })
+  get("/squarert/results", { :controller => "omni", :action => "sq_result" })
+
+  get("/payment", { :controller => "omni", :action => "payment" })
+  get("/payment/results", { :controller => "omni", :action => "pay_result" })
+
+  get("/", { :controller => "omni", :action => "random" })
+  get("/random/results", { :controller => "omni", :action => "rand_result" })
 end
